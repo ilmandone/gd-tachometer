f17bc85d157c1ba986b4fa3f0aa4b14ff4316a4e
Andrea Mandini <a.mandini@mauti.it>
2026-05-25 18:49:41 +0200

    feat (frontend): increase today value by button or keys

--- frontend/src/app/components/main/main.ts
+++ frontend/src/app/components/main/main.ts
@@ -1,9 +1,11 @@
-import { Component, computed, input, signal } from '@angular/core';
+import { Component, computed, HostListener, input, signal } from '@angular/core';
 import { Tachometer } from '../tachometer/tachometer';
 import { NgOptimizedImage } from '@angular/common';
 import { Button } from '../button/button';
 import { CounterService } from '../../services/counter.service';
 import { toSignal } from '@angular/core/rxjs-interop';
+
+type ValueType = 'dog' | 'god'
 
 @Component({
   selector: 'app-main',
@@ -14,7 +16,12 @@ import { toSignal } from '@angular/core/rxjs-interop';
 export class Main {
   private _counterService = new CounterService();
 
+  private _lastValue: ValueType = 'god';
   private _today = toSignal(this._counterService.getToday());
+  private _newValue: Record<ValueType, number> = {
+    god: 0,
+    dog: 0,
+  }
 
   current = computed(() => {
     const today = this._today();
@@ -23,4 +30,38 @@ export class Main {
 
   day = computed(() => this._today()?.date ?? '01/01/1970');
   max = computed(() => this._today()?.limit ?? 100);
+
+  // Update the dog or god looking to the last value
+
+  protected update(value?: ValueType) {
+    const newValue = value ?? (this._lastValue === 'god' ? 'dog' : 'god');
+
+    // TODO UPDATE THE ICON BY THE NEW VALUE
+
+    this._lastValue = newValue
+    this._update(newValue)
+  }
+
+  @HostListener('window:keydown.enter')
+  onEnter() {
+    this.update();
+  }
+
+  @HostListener('window:keydown.d')
+  onD() {
+    this.update('dog');
+  }
+
+  @HostListener('window:keydown.c')
+  onC() {
+    this.update('god');
+  }
+
+  //#region Privates
+
+  private _update(value: ValueType): void {
+    this._newValue[value] += 1
+  }
+
+  //#endregion
 }
