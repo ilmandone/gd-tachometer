c7f0ae5ea3dacf2310ed109d451526573e5c16e3
Andrea Mandini <andrea.mandini@xtel-group.com>
2026-05-26 16:31:49 +0200

    chore (frontend): footer component

---
+++ frontend/src/app/components/footer/footer.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Button } from '../button/button';
+
+@Component({
+  selector: 'footer [app]',
+  imports: [Button],
+  templateUrl: './footer.html',
+  styleUrl: './footer.scss',
+})
+export class Footer {}
