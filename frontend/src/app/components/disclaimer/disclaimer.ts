260e32341fac2e8c4817b296344c1edfc600fdbe
Andrea Mandini <andrea.mandini@xtel-group.com>
2026-05-27 09:00:23 +0200

    fix (frontend): limit button key interaction to space and return

--- frontend/src/app/components/disclaimer/disclaimer.ts
+++ frontend/src/app/components/disclaimer/disclaimer.ts
@@ -1,5 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import {Button} from "../button/button";
+import { filter } from 'rxjs/internal/operators/filter';
+import { fromEvent } from 'rxjs/internal/observable/fromEvent';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'app-disclaimer',
@@ -9,5 +12,21 @@ import {Button} from "../button/button";
 })
 export class Disclaimer {
 
+  private readonly escKeyEvent = fromEvent<KeyboardEvent>(document, 'keydown').pipe(
+    filter(event => event.key === 'Escape')
+  );
+
+  private escKeySubscription!: Subscription ;
   protected readonly showDisclaimer = signal<boolean>(false);
+
+  constructor() {
+    effect(() => {
+      const sd = this.showDisclaimer();
+      if (sd) 
+        this.escKeySubscription = this.escKeyEvent.subscribe(() => this.showDisclaimer.set(false));
+      else 
+        this.escKeySubscription?.unsubscribe();
+      
+    })
+  }
 }
